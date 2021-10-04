d0cc50da056493935aafbfc1d0dfe12d5cf6d535
marco raphael dantas campos <marco-raphael>
2021-10-04 16:06:39 -0300

    Adiçao da pasta aula-variaveis, com os arquivos do trabalho

--- modulo1/aula-variaveis/index.js
+++ modulo1/aula-variaveis/index.js
@@ -3,40 +3,40 @@
 //(20,10,10)
 //p= horasTrabalhadas ,t= pagamentoDiario
 
-/*  1- (a,b,c,d)
+
+/*
+//  1- (a,b,c,d)
 let nome;
 let idade;
 console.log(typeof nome, typeof idade);
 //Foi impresso para as duas variaveis "Undefined", por não ter definido um valor a elas
-*/
 
-/*  1- (e,f,g)
-let nome = prompt ("Qual é o seu nome?")
-let idade = prompt("Qual é a sua idade?")
-console.log(typeof nome, typeof idade); 
+
+// 1- (e,f,g)
+let nome = prompt("Qual é o seu nome?");
+let idade = prompt("Qual é a sua idade?");
+console.log(typeof nome, typeof idade);
 // Foi impresso para ambas as variaveis "String", por serem textos
-console.log("Óla,", nome, ",você tem", idade,"anos" )
-*/
-
-/*  2-
-const blusaAzul = prompt ("Você está de blusa azul?")
-const blusaBranca = prompt ("você está de blusa branca?")
-const blusaPreta = prompt ("você está de blusa preta?")
-console.log ("Você está de blusa azul?",blusaAzul)
-console.log ("Você está de blusa branca?",blusaBranca)
-console.log ("Você está de blusa preta?",blusaPreta)
+console.log("Óla,", nome, ",você tem", idade, "anos");
 */
 
 /*
-let valorDeA = 10
-let valorDeB = 25
-let valorDeC = ""
+//2
+const blusaAzul = prompt("Você está de blusa azul?");
+const blusaBranca = prompt("você está de blusa branca?");
+const blusaPreta = prompt("você está de blusa preta?");
+console.log("Você está de blusa azul?", blusaAzul);
+console.log("Você está de blusa branca?", blusaBranca);
+console.log("Você está de blusa preta?", blusaPreta);
+*/ 
 
-valorDeA = valorDeC
-valorDeB = valorDeA
-valorDeC = valorDeB
-
-console.log("novo valor de a",valorDeA )
-console.log("novo valor de b",valorDeB )
+/*
+//3
+let a = 10;
+let b = 25;
+let c = a;
+a = b;
+b = c;
+console.log("novo valor de a", a);
+console.log("novo valor de b", b);
 */
-
