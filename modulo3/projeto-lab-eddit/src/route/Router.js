91d4ccbb2f9348cf1b5a305fe4e470f4909b958d
marco raphael dantas campos <marco-raphael>
2021-12-15 13:54:34 -0300

    header do mui e rotas no header

--- modulo3/projeto-lab-eddit/src/route/Router.js
+++ modulo3/projeto-lab-eddit/src/route/Router.js
@@ -5,17 +5,19 @@ import LoginPage from "../pages/LoginPage/LoginPage"
 import PostDetailPage from "../pages/PostDetailPage/PostDetailPage"
 import SignUpPage from "../pages/SignUpPage/SignUpPage"
 import ErrorPage from "../pages/ErrorPage/ErrorPage"
+import Header from "../components/Header"
 
 
 const Router = () => {
     return(
         <BrowserRouter>
+        <Header/>
             <Switch>
                 <Route exact path="/cadastro">
                     <SignUpPage/>
                 </Route>
                 
-                <Route exact path="/login">
+                <Route exact path="/Login">
                     <LoginPage/>
                 </Route>
                 
