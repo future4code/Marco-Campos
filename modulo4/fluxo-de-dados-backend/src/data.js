ce30b0d374c0beac66374b4e9d0e10bfff984316
marco raphael dantas campos <marco-raphael>
2022-01-26 23:33:23 -0300

    fluxo de dados backend

---
+++ modulo4/fluxo-de-dados-backend/src/data.js
@@ -0,0 +1,47 @@
+export const data = [
+    {
+        id: "1" ,
+        name: "Bulbasaur" ,
+        price: 10
+    },
+    {
+        id: "2" ,
+        name: "ivysaur" ,
+        price: 20
+    },
+    {
+        id: "3" ,
+        name: "venusaur" ,
+        price: 30
+    },
+    {
+        id: "4" ,
+        name: "charmander" ,
+        price: 10
+    },
+    {
+        id: "5" ,
+        name: "charmeleon" ,
+        price: 20
+    },
+    {
+        id: "6" ,
+        name: "charizard" ,
+        price: 30
+    },
+    {
+        id: "7" ,
+        name: "squirtle" ,
+        price: 10
+    },
+    {
+        id: "8" ,
+        name: "wartortle" ,
+        price: 20
+    },
+    {
+        id: "9" ,
+        name: "blastoise" ,
+        price: 30
+    }
+]
