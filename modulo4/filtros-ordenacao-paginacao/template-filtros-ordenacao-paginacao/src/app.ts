a932f19a486be9046210c34ee6cea4b615f98f1e
marco raphael dantas campos <marco-raphael>
2022-02-08 23:46:43 -0300

    filtros ordenação paginação

---
+++ modulo4/filtros-ordenacao-paginacao/template-filtros-ordenacao-paginacao/src/app.ts
@@ -0,0 +1,12 @@
+import express from "express"
+import cors from "cors"
+import { AddressInfo } from "net"
+
+export const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(3003, () => {
+   console.log(`Server is running in http://localhost:3003`);
+})
---
+++ modulo4/filtros-ordenacao-paginacao/template-filtros-ordenacao-paginacao/src/endpoints/getQueryUsers.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express"
+import { connection } from "../data/connection"
+import { users } from "../types"
+
+export const getQueryUsers = async(req: Request,res: Response): Promise<void> =>{
+   try {
+      const users:users[] = await selectQuery(req.query.name)
+
+      if(!users.length){
+         res.statusCode = 404
+         throw new Error("No recipes found")
+      }
+
+      res.status(200).send(users)
+      
+   } catch (error) {
+      if(error instanceof Error){
+         res.send(error.message)
+      }
+   }
+}
+
+export default async function selectQuery(query: any):Promise<any> {
+   const result = await connection.raw(`
+      SELECT id, name, email, type 
+      FROM aula48_exercicio
+      WHERE nome = ${query};
+   `)
+
+   return result[0]
+}
