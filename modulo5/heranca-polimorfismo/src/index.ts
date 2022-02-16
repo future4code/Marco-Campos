7fde42f9d8dde312d417434ec82aa6254513d874
marco raphael dantas campos <marco-raphael>
2022-02-16 00:55:29 -0300

    heranca polimorfismo

--- modulo5/heranca-polimorfismo/src/index.ts
+++ modulo5/heranca-polimorfismo/src/index.ts
@@ -1,6 +1,10 @@
-
-console.log("Hello, world!")
+import { Customer } from "./Customer"
+import { User } from "./User"
 
 const marco = new User("001","marco@marco.com","marco","marco123")
 
 console.log(marco)
+
+const fran = new Customer("002","fran@fran.com","fran","fran123","123456789")
+
+console.log(fran)
