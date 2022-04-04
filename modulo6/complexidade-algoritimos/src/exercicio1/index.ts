5380c1b3ee78af0e57ec976187e5df6a39484467
marco raphael dantas campos <marco-raphael>
2022-04-04 03:49:55 -0300

    complexidade de algoritimos

---
+++ modulo6/complexidade-algoritimos/src/exercicio1/index.ts
@@ -0,0 +1,13 @@
+// const findFirstRecurringCharacter = (input) => {
+//     const charHashMap = {};
+//     for (const char of input) {
+//       if (charHashMap[char] === true) {
+//         return char;
+//       }
+//       charHashMap[char] = true;
+//     }
+//     return null;
+//   }; 
+
+
+// resposta: O(n)
